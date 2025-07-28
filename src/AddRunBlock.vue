bd92740b2489f5637841ca99ac473e292270e0d0
wwqwsff <sam.galoa@mail.ru>
2025-07-28 16:48:00 +0500

    верстка

---
+++ src/AddRunBlock.vue
@@ -0,0 +1,77 @@
+<script setup>
+const props = defineProps({
+    text2:{
+        type: String,
+        default:'км:'
+    },
+    text3: {
+        type: String,
+        default:'Введите данные'
+    },
+    writebig:{
+        type: String,
+        default:'label'
+    },
+    bigcont:{
+        type: String,
+        default:'input-style"'
+    }
+})
+</script>
+
+<template>
+     <div class="base-form">
+       <div class="label" :class="[writebig]">{{ text2 }}</div> 
+
+        <div class="bace-form-input">
+            <input type="text" class="input-style" :placeholder="text3" :class="[bigcont]">
+        </div>
+     </div>
+</template>
+
+<style scoped>
+.input-style{
+    height: 30px;
+    width: 130px;
+    padding: 5px;
+    box-sizing: border-box;
+    background-color:#F0E68C;
+    border-radius: 6px;
+    border: 1px solid black; 
+}
+
+
+.input-style::placeholder {
+    color: black;
+    opacity: 1; 
+}
+
+.base-form{
+    display: flex;
+    align-items: center; 
+    gap: 60px; 
+    margin-bottom: 27px;
+    
+}
+.label {
+    width: 50px; 
+    text-align: left;
+    position: relative;
+    top: 2px; 
+    left: 30px;
+    color: black;
+}
+.bigtext{
+    font-size: 20px;
+}
+.big{
+  height: 30px;
+  width: 170px;
+  padding: 5px;
+  box-sizing: border-box;
+  background-color: #F0E68C;
+  border-radius: 6px;
+  margin-left: 30px; /* Добавляем отступ слева */
+  border: 1px solid black;   
+}
+</style>
---
+++ src/Dropwindow.vue
@@ -0,0 +1,90 @@
+<script setup>
+const props = defineProps({
+    
+    variation: {
+        type: String,
+        default: 'Тип'
+    },
+    look: {
+        type: String,
+        default: '1'
+    },
+     text1: {
+        type: String,
+        default: '1'
+    },
+     text3: {
+        type: String,
+        default: '1'
+    },
+     text4: {
+        type: String,
+        default: '1'
+    },
+     text2: {
+        type: String,
+        default: '1'
+    },
+       sizebig: {
+        type: String,
+        default: 'add-run-select'
+    },
+    writebig: {
+        type: String,
+        default: 'add-run-input-type'
+    }
+})
+defineEmits(['click'])
+</script>
+<template>
+<div class="add-run-sel">
+    <div class="add-run-input-type" :class="[writebig]">{{ variation }} </div>  
+    <select class="add-run-select" :class="[sizebig]">
+        <option value="">{{ look }}</option>
+        <option value="1">{{ text1 }}</option>
+        <option value="2">{{ text2 }}</option>
+        <option value="3">{{ text3 }}</option>
+        <option value="4">{{ text4 }}</option>
+    </select>
+</div>
+    </template>
+
+<style scoped>
+.add-run-sel{
+   display: flex;
+    align-items: center; 
+    gap: 20px; 
+    margin-bottom: 27px;
+}
+.add-run-input-type {
+  width: auto; /* Убираем фиксированную ширину */
+  text-align: left;
+  color: black;
+  margin-left: 30px; /* Заменяем left на margin */
+  
+}
+.add-run-select {
+  height: 30px;
+  width: 130px;
+  padding: 5px;
+  box-sizing: border-box;
+  background-color: #F0E68C;
+  border-radius: 6px;
+  margin-left: 30px; /* Добавляем отступ слева */
+  border: 1px solid black; 
+}
+.big{
+  height: 30px;
+  width: 170px;
+  padding: 5px;
+  box-sizing: border-box;
+  background-color: #F0E68C;
+  border-radius: 6px;
+  margin-left: 30px; /* Добавляем отступ слева */
+  border: 1px solid black;   
+}
+.bigtext{
+    font-size: 20px;
+    margin-left: 70px;
+}
+</style>
